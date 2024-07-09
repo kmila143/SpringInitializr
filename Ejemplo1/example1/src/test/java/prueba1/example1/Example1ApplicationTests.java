2ee5e106d603bbdc1fca43d18375c4592a5fe275
kmi143 <cuclibni@gmail.com>
2024-07-08 23:09:49 -0600

    Initial commit with base Spring Boot project and HealthController

---
+++ Ejemplo1/example1/src/test/java/prueba1/example1/Example1ApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba1.example1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
